9e5a6018effb3c4e9cfc0f4adc843ea7bd4cdfb5
Kelley Lam <kxl1360@rit.edu>
2020-11-18 21:33:47 -0500

    change return format to be panda tables

--- rocketTools.py
+++ rocketTools.py
@@ -8,6 +8,10 @@ conn = psycopg2.connect(host="reddwarf@cs.rit.edu", port = 5432, database="p320_
 #create cursor obj
 cur = conn.cursor()
 
+def createDataDisplay(sql_query, database = conn):
+    table = pd.read_sql_query(sql_query, database)
+    return table
+
 def run():
 	""" Function to handle command line usage"""
 	args = sys.argv
@@ -17,32 +21,19 @@ def run():
 	else:
 		for a in args:
 			if a == '--help':
-				print('Basic command line program')
+				print('RocketTools Command Line Program')
 				print('Options:')
 				print('    --help -> show this basic help menu.')
 				print('    --ol -> show owner leaderboard')
 				print('    --bl -> show borrower leaderboard')
 			elif a == '--ol':
-				cur.execute("""SELECT u.name, own.cnt
-							FROM "user" u
-							JOIN (SELECT "ownerUNR", count("ownerUNR") AS cnt
-									FROM "ownedTool"
-									GROUP BY "ownerUNR") own ON u.unr = own."ownerUNR"
-							ORDER BY cnt DESC
-							LIMIT 10;""")
-				query_results = cur.fetchall()
-				print(query_results)
+				createDataDisplay("SELECT u.name, own.cnt FROM "user" u JOIN (SELECT "ownerUNR", count("ownerUNR") AS cnt FROM "ownedTool" GROUP BY "ownerUNR") own ON u.unr = own."ownerUNR" ORDER BY cnt DESC LIMIT 10;")
 			elif a == '--bl':
-				cur.execute("""SELECT u.name, borrow.cnt
-							FROM "user" u
-							JOIN (SELECT "borrowerUNR", count("borrowerUNR") AS cnt
-									FROM "borrowedTool"
-									GROUP BY "borrowerUNR") borrow ON u.unr = "borrowerUNR"
-							ORDER BY cnt DESC
-							LIMIT 10;""")
-				query_results = cur.fetchall()
-				print(query_results)
+				createDataDisplay("SELECT u.name, borrow.cnt FROM "user" u JOIN (SELECT "borrowerUNR", count("borrowerUNR") AS cnt FROM "borrowedTool" GROUP BY "borrowerUNR") borrow ON u.unr = "borrowerUNR" ORDER BY cnt DESC LIMIT 10;")
 			else:
 				print('Unrecognised argument.')
 if __name__ == '__main__':
 	do_work()
+
+cur.close()
+conn.close()
